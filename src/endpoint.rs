f8ecd21f0e6a346743b32de0e3d9670cf540e322
David Petrizze <dpetrizze@gmail.com>
2023-10-30 19:47:31 -0600

    Implement drop detection in segment tx window

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -35,7 +35,7 @@ pub trait HostContext {
     fn on_timeout(&mut self);
 }
 
-struct FrameTxWindow {
+struct SegmentTxWindow {
     //   base    next    base+size
     //   v       v       v
     // --########________--------> frame IDs
@@ -49,9 +49,11 @@ struct FrameTxWindow {
     // sizes of [base, next)
     transit_buf: VecDeque<usize>, // TODO: Static buffer
     transit_total: usize,
+
+    ack_history: u32,
 }
 
-impl FrameTxWindow {
+impl SegmentTxWindow {
     pub fn new(base_id: u32, size: u32) -> Self {
         Self {
             base_id,
@@ -60,7 +62,13 @@ impl FrameTxWindow {
 
             transit_buf: VecDeque::new(),
             transit_total: 0,
+
+            ack_history: u32::MAX,
         }
+    }
+
+    fn delta(&self, segment_id: u32) -> u32 {
+        segment_id.wrapping_sub(self.base_id)
     }
 
     fn next_delta(&self) -> u32 {
@@ -72,7 +80,7 @@ impl FrameTxWindow {
     }
 
     pub fn can_send(&self) -> bool {
-        self.next_delta() < self.size
+        self.delta(self.next_id) < self.size
     }
 
     pub fn mark_sent(&mut self, size: usize) {
@@ -87,28 +95,201 @@ impl FrameTxWindow {
         self.transit_total
     }
 
-    /// Acknowledges previously sent frames by indicating the receiver's next expected frame
-    pub fn acknowledge(&mut self, frame_id: u32) -> bool {
-        let frame_delta = frame_id.wrapping_sub(self.base_id);
+    /// Acknowledges previously sent frames
+    pub fn acknowledge(
+        &mut self,
+        rx_base_id: u32,
+        rx_history: u32,
+        _rx_history_nonce: bool,
+    ) -> bool {
+        let ack_delta = self.delta(rx_base_id);
 
-        // Do nothing unless:
-        //   * frame_id represents an advancement,
-        //   * frame_id acknowledges at most the entire current window, and
-        //   * frame_id does not exceed the next frame ID which will be sent
-        if frame_delta > 0 && frame_delta <= self.size && frame_delta <= self.next_delta() {
-            self.base_id = frame_id;
+        // The delta ack must not exceed the next ID to be sent, else it is invalid
+        if ack_delta <= self.delta(self.next_id) {
+            if ack_delta > 0 {
+                // Advance the segment tx window
+                self.base_id = rx_base_id;
 
-            for size in self
-                .transit_buf
-                .drain(0..usize::try_from(frame_delta).unwrap())
-            {
-                self.transit_total -= size;
+                // Anything between the current base_id and the acknowledged base_id has very likely
+                // left the network; subtract from running total
+                for size in self
+                    .transit_buf
+                    .drain(0..usize::try_from(ack_delta).unwrap())
+                {
+                    self.transit_total -= size;
+                }
             }
 
-            true
-        } else {
-            false
+            // This history queue incrementally tracks acknowledged segments from the receiver
+            // until a drop is detected. A drop is detected when three segments have been received
+            // after an unacknowledged segment, or any unacknowledged segment is behind the base ID
+            // by ≥32 IDs. When a drop is detected, the queue resets to the default state where all
+            // segments have been acknowledged in order to detect the next drop.
+
+            //                     base (prev)    base' (latest ack)
+            //                     v              v
+            // xxxxxxxx xxxxxxxx xx000000 00000000   self.ack_history
+            //                     000000 000yyyyy   rx_history
+
+            let N = u32::BITS;
+
+            if ack_delta > self.ack_history.leading_ones() {
+                // Shifting would cause a zero to be >32 bits behind, that's a drop
+                self.ack_history = u32::MAX;
+                return true;
+            }
+
+            // Shift in zeros corresponding to this delta
+            if ack_delta == N {
+                self.ack_history = 0;
+            } else {
+                self.ack_history = self.ack_history.wrapping_shl(ack_delta);
+            }
+
+            // TODO: Validate rx_history_nonce against sent nonces
+
+            // Add received history bits to history buffer
+            self.ack_history |= rx_history;
+
+            if self.ack_history == u32::MAX {
+                // All segments accounted for, not a drop
+                return false;
+            }
+
+            if self.ack_history.wrapping_shr(N - 1) == 0 {
+                // Oldest unacknowledged segment is 32 IDs old, that's a drop
+                self.ack_history = u32::MAX;
+                return true;
+            }
+
+            let leading_ones = self.ack_history.leading_ones();
+            debug_assert!(leading_ones < N);
+
+            // Count the ones after the first zero
+            let ones_after_zero = self
+                .ack_history
+                .wrapping_shl(leading_ones)
+                .wrapping_shr(leading_ones)
+                .count_ones();
+
+            if ones_after_zero >= 3 {
+                // Three segments have been reported since the first unacknowledged segment, that's
+                // equivalent to a TCP NDUPACK=3 condition
+                self.ack_history = u32::MAX;
+                return true;
+            } else {
+                // Not a drop, yet
+                return false;
+            }
         }
+
+        // Invalid ack, not a drop
+        return false;
+    }
+}
+
+#[cfg(test)]
+mod tests2 {
+    use super::*;
+
+    #[test]
+    fn send_receive_window() {
+        let mut tx = SegmentTxWindow::new(0, 128);
+
+        for i in 0..128 {
+            assert_eq!(tx.can_send(), true);
+            assert_eq!(tx.next_id(), i);
+            assert_eq!(tx.bytes_in_transit(), (i * 5) as usize);
+            tx.mark_sent(5);
+        }
+
+        assert_eq!(tx.can_send(), false);
+
+        for i in 0..128 {
+            assert_eq!(tx.acknowledge(i + 1, 0x1, false), false);
+            assert_eq!(tx.bytes_in_transit(), ((128 - i - 1) * 5) as usize);
+            assert_eq!(tx.can_send(), true);
+        }
+    }
+
+    fn new_filled_window(size: usize) -> SegmentTxWindow {
+        let mut tx = SegmentTxWindow::new(0, 128);
+
+        for _ in 0..size {
+            tx.mark_sent(0);
+        }
+
+        tx
+    }
+
+    #[test]
+    fn drop_detection() {
+        const WINDOW_SIZE: usize = 128;
+
+        const DUMMY_NONCE: bool = false;
+
+        // Drop declared if 3 segments are acked after the first unacked segment
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b01001, DUMMY_NONCE), false);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b01001, DUMMY_NONCE), false);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b01101, DUMMY_NONCE), true);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b10011, DUMMY_NONCE), false);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b10111, DUMMY_NONCE), true);
+
+        // History can be updated without causing a drop
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b10011, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(5, 0b11011, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(7, 0b11101, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(7, 0b11111, DUMMY_NONCE), false);
+
+        // History can be updated and cause a drop
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(5, 0b10011, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(7, 0b11111, DUMMY_NONCE), true);
+
+        // Maximum gap between acknowledged segments
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(32, (0b1 << 31) | 0b001, DUMMY_NONCE), false);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(32, (0b1 << 31) | 0b011, DUMMY_NONCE), false);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(32, (0b1 << 31) | 0b111, DUMMY_NONCE), true);
+
+        // Maximum possible acknowledgement, 32 segments total
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(32, u32::MAX, DUMMY_NONCE), false);
+
+        // Past-maximum acknowledgement always fails
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(33, u32::MAX, DUMMY_NONCE), true);
+
+        // Any unacknowledged segment older than 32 is a drop, even if < 3 acked thereafter
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(31, 0b0, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(32, 0b0, DUMMY_NONCE), true);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(32, 0b1, DUMMY_NONCE), true);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(16, 0b1, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(32, 0b1, DUMMY_NONCE), true);
+
+        let mut tx = new_filled_window(WINDOW_SIZE);
+        assert_eq!(tx.acknowledge(16, 0b1, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(31, 0b1, DUMMY_NONCE), false);
+        assert_eq!(tx.acknowledge(32, 0b0, DUMMY_NONCE), true);
     }
 }
 
@@ -272,7 +453,7 @@ const RESEND_TIMEOUT_MS: u64 = 3000;
 
 pub struct Endpoint {
     // Sender state
-    tx_window: FrameTxWindow,
+    segment_tx_window: SegmentTxWindow,
     tx_data: FragmentTxBuffers,
 
     cwnd: usize,
@@ -291,14 +472,15 @@ impl Endpoint {
     pub fn new() -> Self {
         let fragment_size = 1478;
         let fragment_window_size = 1024;
+        let segment_window_size = 128;
 
         Self {
-            tx_window: FrameTxWindow::new(0, 20),
+            segment_tx_window: SegmentTxWindow::new(0, segment_window_size),
             tx_data: FragmentTxBuffers::new(0, fragment_window_size, fragment_size),
             cwnd: 15000,
             tx_buffer: [0; 1478],
             rto_timer_set: false,
-            segment_rx_buffer: buffer::SegmentRxBuffer::new(0, 20, fragment_size),
+            segment_rx_buffer: buffer::SegmentRxBuffer::new(0, segment_window_size, fragment_size),
             unreliable_rx: buffer::UnreliableRxBuffer::new(0, fragment_window_size, fragment_size),
             reliable_rx: buffer::ReliableRxBuffer::new(0, fragment_size),
         }
@@ -341,12 +523,18 @@ impl Endpoint {
 
         if read_ack {
             if let Some(stream_ack) = frame_reader.read::<frame::StreamAck>() {
-                self.tx_window.acknowledge(stream_ack.data_id);
+                if self
+                    .segment_tx_window
+                    .acknowledge(stream_ack.data_id, 0x1F, false)
+                {
+                    println!("drop detected");
+                }
 
-                self.tx_data.acknowledge(stream_ack.unrel_id, stream_ack.rel_id);
+                self.tx_data
+                    .acknowledge(stream_ack.unrel_id, stream_ack.rel_id);
 
                 if self.rto_timer_set {
-                    if stream_ack.data_id == self.tx_window.next_id() {
+                    if stream_ack.data_id == self.segment_tx_window.next_id() {
                         // All caught up
                         ctx.unset_timer(TimerName::Rto);
                         self.rto_timer_set = false;
@@ -461,8 +649,8 @@ impl Endpoint {
             );
             */
 
-            let frame_window_limited = !self.tx_window.can_send();
-            let congestion_window_limited = self.tx_window.bytes_in_transit() >= self.cwnd;
+            let frame_window_limited = !self.segment_tx_window.can_send();
+            let congestion_window_limited = self.segment_tx_window.bytes_in_transit() >= self.cwnd;
             let data_ready = self.tx_data.data_ready();
 
             let send_data = !frame_window_limited && !congestion_window_limited && data_ready;
@@ -516,7 +704,7 @@ impl Endpoint {
 
                 if send_data {
                     let header = frame::StreamDataHeader {
-                        id: self.tx_window.next_id(),
+                        id: self.segment_tx_window.next_id(),
                     };
 
                     frame_writer.write(&header);
@@ -525,7 +713,7 @@ impl Endpoint {
 
                     debug_assert!(size_written > 0);
 
-                    self.tx_window.mark_sent(size_written);
+                    self.segment_tx_window.mark_sent(size_written);
 
                     // Acks are expected, set the resend timeout if not already
                     if !self.rto_timer_set {
