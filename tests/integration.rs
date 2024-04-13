b829131cd1b8dba326cc06d25594c2d9c92cbcb3
David Petrizze <dpetrizze@gmail.com>
2024-04-13 16:41:29 -0600

    Add Send trait tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -75,4 +75,34 @@ mod integration {
 
         run(config);
     }
+
+    #[test]
+    fn client_send() {
+        let server_port = 10004;
+
+        // Create client object in main thread to verify it implements Send
+        let mut client = redpine::Client::connect(("127.0.0.1", server_port))
+            .expect("failed to create redpine client");
+
+        let thread = std::thread::spawn(move || {
+            client.poll_event();
+        });
+
+        thread.join().unwrap();
+    }
+
+    #[test]
+    fn server_send() {
+        let server_port = 10004;
+
+        // Create server object in main thread to verify it implements Send
+        let mut server = redpine::Server::bind(("127.0.0.1", server_port))
+            .expect("failed to create redpine server");
+
+        let thread = std::thread::spawn(move || {
+            server.poll_event();
+        });
+
+        thread.join().unwrap();
+    }
 }
